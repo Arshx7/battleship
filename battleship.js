f11faff0f2e78ea3dde1e77eedda28d5b1541acc
Arshakh Haneen <arshx77@gmail.com>
2024-10-24 12:51:51 +0530

    Add the Gameboard and Player class

--- battleship.js
+++ battleship.js
@@ -7,18 +7,75 @@ class Ship {
 
     hit() {
         this.hits++;
-        return this.hits;
+        return this.hits
     }
 
     isSunk() {
-        if(this.length <= this.hits) {
-            this.sunk = true;
+        return this.hits >= this.length;
+    }
+}
+
+class Gameboard {
+    constructor() {
+        this.shipList = [];
+        this.trackMissed = [];
+    }
+    
+    placeShip(ship, coordinate, orientation) {
+        let shipSize = [];
+        
+        if(orientation === "vertical") {
+            for(let i = 0; i < ship.length; i++) {
+
+                let newCoordinate = [coordinate[0], coordinate[1] + i];
+                shipSize.push(newCoordinate)
+            }
             
-        } 
-        return this.sunk;
+        } else {
+            for(let i = 0; i < ship.length; i++) {
+                
+                let newCoordinate = [coordinate[0] + i, coordinate[1]];
+                shipSize.push(newCoordinate)
+            }
+
+        }
+        this.shipList.push({ ship, coordinates: shipSize });
+    }
+
+    receiveAttack(coordinate) {
+        let hit = false;
+        this.shipList.forEach(item => {
+            if(item.coordinates.some(pos => pos[0] === coordinate[0] && pos[1] === coordinate[1])) {
+                item.ship.hit();
+                hit = true;
+                
+            } 
+        });
+        if(!hit) {
+            this.trackMissed.push(coordinate);
+        }
+        
+    }
+
+    allShipSunk() {
+        return this.shipList.every(item => item.ship.isSunk());
+    }
+}
+
+class Player {
+    constructor(name) {
+        this.name = name;
+        this.gameboard = new Gameboard()
+    }
+
+    placeShip(ship, coordinate, orientation) {
+        this.gameboard.placeShip(ship, coordinate, orientation);
+    }
+
+    attack(opponent, coordinate) {
+        opponent.gameboard.receiveAttack(coordinate);
     }
 }
 
 
-
-module.exports = { Ship };
+module.exports = { Ship, Gameboard, Player };
